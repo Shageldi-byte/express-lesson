024dda07dc38711abedcf63c3cf54b5e7b3d5031
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-04-20 15:57:53 +0500

    bcrypt

--- src/auth/auth.controller.mjs
+++ src/auth/auth.controller.mjs
@@ -1,10 +1,13 @@
 import express from "express";
-import { getMe, login } from "./auth.service.mjs";
+import { getMe, login, signIn, signUp } from "./auth.service.mjs";
 import { authenticateToken } from "./jwt.service.mjs";
 
 const authController = express.Router();
 
 authController.post('/login',login);
+
+authController.post('/sign-up',signUp);
+authController.post('/sign-in',signIn);
 authController.get('/get-me',authenticateToken,getMe);
 
 export default authController;
--- src/auth/auth.service.mjs
+++ src/auth/auth.service.mjs
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { responseGenerator } from "../core/app.response.mjs";
@@ -38,6 +39,73 @@ export function login(req,res){
     })
 }
 
+async function checkUser(password,hash) {
+    return await bcrypt.compare(password, hash);
+}
+
+
+function createUser(user,res){
+    db.query(`INSERT INTO second.users(
+	fullname, username, password, dob)
+	VALUES ($1,$2,$3,$4) RETURNING *;`,[user.fullname,user.username,user.password,user.dob])
+    .then(result=>{
+        res.json(responseGenerator(result.rows[0]));
+    })
+    .catch(err=>{
+        console.log(err);
+        badRequest(res)
+    })
+}
+
+export async function signUp(req,res){
+    const saltRounds = 10;
+
+    const {
+        fullname,
+        username,
+        password,
+        dob
+    } = req.body;
+    
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        console.log(err);
+        bcrypt.hash(password, salt, function(err, hash) {
+            createUser({fullname,username,dob,password:hash},res);
+        });
+    });
+
+  
+
+
+}
+
+export async function signIn(req,res){
+    const {
+        username,password
+    } = req.body;
+
+    db.query(`SELECT * FROM second.users WHERE username=$1 ORDER BY created_at DESC LIMIT 1;`,[username])
+    .then(async result=>{
+        if(result.rows.length>0){
+            let match = await checkUser(password,result.rows[0].password);
+            if(match){
+                let user = result.rows[0];
+                user.password = "";
+                res.json(responseGenerator(user));
+            } else {
+                badRequest(res,{en:'Invalid password'},403);
+            }
+        } else {
+            badRequest(res,'user not found');
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        badRequest(res);
+    })
+}
+
 export function getMe(req,res){
     res.json(req.user);
 }
+
