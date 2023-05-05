5daac6f8d9fe44f2bddf9a5c244f5f7988cc8d95
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-05-05 23:34:25 +0500

    search added

--- src/parse/parse.controller.mjs
+++ src/parse/parse.controller.mjs
@@ -1,8 +1,8 @@
 import express from "express";
-import { parseTmCars } from "./parse.service.mjs";
+import { parseTmCars, searchFromGoogle } from "./parse.service.mjs";
 
 const parseController = express.Router();
 
 parseController.get('/get-news-from-tmcars',parseTmCars);
-
+parseController.get('/search-from-google',searchFromGoogle);
 export default parseController;
